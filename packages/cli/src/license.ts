f54ecd3208f5dd34020a473d6c23f531e735eb90
Reinakumiko <Reinakumiko@users.noreply.github.com>
2025-12-23 23:20:49 +0800

    Modify isLicensed to always return true
    Changed the isLicensed method to always return true.

--- packages/cli/src/license.ts
+++ packages/cli/src/license.ts
@@ -250,7 +250,7 @@ export class License implements LicenseProvider {
 	}
 
 	isLicensed(feature: BooleanLicenseFeature) {
-		return this.manager?.hasFeatureEnabled(feature) ?? false;
+		return true;
 	}
 
 	/** @deprecated Use `LicenseState.isDynamicCredentialsLicensed` instead. */
